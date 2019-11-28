81acc1d81b9b6fc6d25a8f07ab17b29411e43d14
yuzhiwang <yuzhiwang@tencent.com>
2019-11-28 17:05:16 +0800

    gomod v3 commit

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/xgdwq/gomod
+module github.com/xgdwq/gomod/v3
 
 go 1.13
--- hello.go
+++ hello.go
@@ -3,3 +3,7 @@ package gomod
 func Hello() string {
 	return "v2: Hello World!"
 }
+
+func HelloV3() string {
+	return "v3.HelloV3: Hello World!"
+}
