4937863d6b0e31834910964936a947cd82b324ba
yuzhiwang <yuzhiwang@tencent.com>
2019-11-28 16:21:38 +0800

    gomod v2

--- hello.go
+++ hello.go
@@ -1,5 +1,5 @@
 package gomod
 
 func Hello() string {
-	return "Hello World!"
+	return "v2: Hello World!"
 }
--- hello_test.go
+++ hello_test.go
@@ -3,7 +3,7 @@ package gomod
 import "testing"
 
 func TestHello(t *testing.T) {
-	want := "Hello World!"
+	want := "v2: Hello World!"
 	if Hello() != want {
 		t.Errorf("Hello() != %s", want)
 	}
